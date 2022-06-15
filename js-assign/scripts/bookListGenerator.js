c8d521868c7a20880a5f1497679c8d95a9c5442c
nikhilatcompro <107535719+nikhilatcompro@users.noreply.github.com>
2022-06-15 12:24:15 +0530

    changed "genreList" to "list" in randomItem function.

--- js-assign/scripts/bookListGenerator.js
+++ js-assign/scripts/bookListGenerator.js
@@ -1,5 +1,5 @@
 function randomItem(list){
-	return Math.floor(Math.random()*genreList.length);
+	return Math.floor(Math.random()*list.length);
 }
 
 const myBookList = [];
@@ -7,20 +7,19 @@ const priceList=[100, 200, 150, 300, 500, 1000, 50, 159, 180];
 const genreList = ['thriller', 'horror', 'romance', 'adventure', 
 'mystery', 'crime', 'political', 'biography', 'fairytale', 'forkids'];
 
-for(let i=0;i<100;i++){
-    let obj = {
-        bookId: i,
-        genre: genreList[randomItem(genreList)],
-        price: priceList[randomItem(priceList)]
-    }
-
-    myBookList.push(obj);
+for(let i=0; i<100; i++){
+	let obj = {
+		bookId: i,
+		genre: genreList[randomItem(genreList)],
+    price: priceList[randomItem(priceList)]
+	}
+	myBookList.push(obj);
 }
 
 const bookListJSON = JSON.stringify(myBookList);
 
 let fs = require('fs');
-fs.writeFile("bookList3.json", bookListJSON, function(err) {
-    if (err) { console.log(err); }
+fs.writeFile("bookList.json", bookListJSON, function(err) {
+  if (err) { console.log(err); }
 });
 
